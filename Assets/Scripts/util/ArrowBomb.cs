281e688cc9946225ad36a38e643ef094730bf6e5
AslakV <aslak_vanggaard@hotmail.com>
2014-05-10 10:53:54 +0200

    added arrowbomb

--- Assets/Scripts/util/ArrowBomb.cs
+++ Assets/Scripts/util/ArrowBomb.cs
@@ -1,15 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ArrowBomb : MonoBehaviour {
+public class ArrowBomb : MonoBehaviour, AlarmListener {
+	float angle = 0;
+	Alarm alarm;
 
 	// Use this for initialization
 	void Start () {
-	
+		alarm = GetComponent<Alarm> ();
+		alarm.setListener (this);
+		//StartCoroutine(animationBFexplode());
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		alarm.addTimer (1, 2, false);
 	}
+
+	public void onAlarm(int i){
+		alarm.addTimer (1, 2, false);
+		Debug.Log (i);
+	}
+
+	/*public IEnumerator animationBFexplode(){
+
+		bool flashSwitch = false;
+		for (var j = 0; j < 4; j++) {
+			if (flashSwitch){
+				renderer.material.color = Color.blue;
+				flashSwitch = true;
+			}
+			else{
+				renderer.material.color = Color.white;
+				flashSwitch = false;
+			}
+			yield return new WaitForSeconds(0.5f);
+
+				}
+			arrowBombExplode();
+		}
+
+	public void arrowBombExplode(){
+
+		for (var i = 12; i > 0; i--) {
+		Instantiate (Resources.Load ("ArrowBombShot"), transform.position + new Vector3(0, 0), Quaternion.Euler(0,0,angle));
+			angle += 30f;
+		}
+	}*/
 }
---
+++ Assets/Scripts/util/ArrowBombShot.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowBombShot : MonoBehaviour {
+	private float speed = 1f;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (speed * Time.deltaTime, 0, 0);
+	}
+}
