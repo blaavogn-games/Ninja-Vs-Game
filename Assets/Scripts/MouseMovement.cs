a2cb7126269c29c5d79129b80d2ee9e0ca436180
SuperPaw <hovsgaardfilm@youseeme.dk>
2014-05-10 14:48:32 +0200

    Mouse traction og boundary tilføjet til at cleare objekter, der kommer uden for spilområdet.

--- Assets/Scripts/MouseMovement.cs
+++ Assets/Scripts/MouseMovement.cs
@@ -9,17 +9,32 @@ public class MouseMovement : MonoBehaviour {
 	public GameObject spreadBomb;
 	float bulletTime= 0;
 	public float fireRate;
+	GUILayer test;
+	Vector3 deltaPos;
+	Vector3 lastPos;
 
 
 
 	// Use this for initialization
 	void Start () {
-		Screen.showCursor = (false);
+		Screen.showCursor = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButton(0) && Time.time > bulletTime) {
+		mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		
+		deltaPos = mousePosition - lastPos;
+		
+		wobbleX = Mathf.Sin(Time.time) + deltaPos.x;
+		wobbleY = Mathf.Cos(Time.time) + deltaPos.y;
+
+		this.transform.position = new Vector3 (mousePosition.x + wobbleX, mousePosition.y + wobbleY, 0);
+		
+		
+		Debug.Log (deltaPos.x);
+
+			if (Input.GetMouseButton(0) && Time.time > bulletTime) {
 			bulletTime = Time.time +fireRate;
 			Instantiate (spreadBomb, this.transform.position, Quaternion.identity);
 			
@@ -27,20 +42,16 @@ public class MouseMovement : MonoBehaviour {
 			Debug.Log (bulletTime+" = bulletTime");
 		}
 
+		lastPos = this.transform.position;
 
-
-		wobbleX = Mathf.Sin(Time.time *3);
-		wobbleY = Mathf.Cos(Time.time * 2);
 	
 		//GUI.DrawTexture (Rect(Input.mousePosition.x, Input.mousePosition.y, 32, 32,myCursor);
-	
-		mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-		this.transform.position = new Vector3(mousePosition.x + wobbleX, mousePosition.y + wobbleY, 0);
-
 		 //= Rect( Input.mousePosition.x - (crosshairTexture.width / 2), (Screen.height - Input.mousePosition.y) - (crosshairTexture.height / 2), crosshairTexture.width , crosshairTexture.height );
 
 
 
 
 	}
-}
+
+	}
+	
