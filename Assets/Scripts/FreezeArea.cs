e789bb074f8d517bbffb462e04649b22e6110b46
AslakV <aslak_vanggaard@hotmail.com>
2014-05-11 11:14:16 +0200

    added swamp animation

--- Assets/Scripts/FreezeArea.cs
+++ Assets/Scripts/FreezeArea.cs
@@ -1,16 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FreezeArea : MonoBehaviour {
+public class FreezeArea : MonoBehaviour, AlarmListener {
+	public Sprite slowFrame1, slowFrame2, slowFrame3, slowFrame4;
     SpriteRenderer spriteRenderer;
     float meltTime = 0;
+	bool aniDir = true;
 
     void Start() {
-        spriteRenderer = GetComponent<SpriteRenderer>();
+		alarm = GetComponent<Alarm> ();
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+		if (alarm == null) {
+			alarm = gameObject.AddComponent<Alarm>();		
+		}
+		alarm.setListener (this);
+		alarm.addTimer (0.003f, 1, false);
     }
 
 	// Update is called once per frame
 	void Update () {
+
+		spriteRenderer.sprite
         meltTime += Time.deltaTime;
         float trans = (meltTime < 4) ? 1 : 5 - meltTime;
 
@@ -18,4 +28,35 @@ public class FreezeArea : MonoBehaviour {
        // if (meltTime >= 5)
        //     Destroy(gameObject);
     }
-}
+
+	public void onAlarm(int i){
+		float aniSpeed = 0.3;
+		if (i == 1) {
+			spriteRenderer.sprite = slowFrame1;
+			aniDir = true;
+			Alarm(aniSpeed, 2, false)
+				}
+		else if (i == 2){
+			spriteRenderer.sprite = slowFrame1;
+			if (aniDir == true){
+				Alarm(aniSpeed, 3, false)
+			}
+			else{
+				Alarm(aniSpeed, 1, false)
+			}
+		}
+		else if (i == 3){
+			spriteRenderer.sprite = slowFrame3;
+			if (aniDir == true){
+				Alarm(aniSpeed, 4, false)
+			}
+			else{
+				Alarm(aniSpeed, 2, false)
+			}
+		}
+		else if (i == 4){
+			spriteRenderer.sprite = slowFrame4;
+			aniDir = false;
+			Alarm(0.2, 3, false)
+		}
+	}
